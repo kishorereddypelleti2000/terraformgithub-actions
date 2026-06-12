5e6f822cc213529e1bebbfdcc06a07ba929ce260
KISHORE REDDY PELLETI <kishorereddypelleti12@gmail.com>
2026-06-12 10:52:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-0152204c1a187337c"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Github-Actions-EC2"
+  }
+}
